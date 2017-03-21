551d56348efe7eea77ade953b807d22dc91b97c2
anderuraga <ander.uraga@gmail.com>
2017-03-21 18:16:47 +0100

    nuevo package para jonantu1989

---
+++ src/com/ipartek/formacion/guia/FranciaLandas.java
@@ -0,0 +1,8 @@
+package com.ipartek.formacion.guia;
+
+public class FranciaLandas {
+
+	public static void main(String[] args) {
+		System.out.println("Listado de todas las playas de Francia Landas");
+	}
+}
