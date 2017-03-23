7771558f98950cabcc430c79c66afc2c19875d17
Amartinezalbala <martinezalbalaalvaro@gmail.com>
2017-03-23 19:34:25 +0100

    Inicio de los trabajos de clase

---
+++ src/com/ipartek/formacion/alvaromartinez/CantabriaSantander.java
@@ -0,0 +1,12 @@
+package com.ipartek.formacion.alvaromartinez;
+
+public class CantabriaSantander {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Listado de playas de Santander(Cantabria): ");
+		
+
+	}
+
+}
