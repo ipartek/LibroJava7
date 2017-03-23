75332767bae284b821838f2cc0031dc02c667ec2
IJPazos <saga.1516@gmail.com>
2017-03-23 17:26:07 +0100

    Subida de @IJPazos

---
+++ src/com/ipartek/formacion/IsaacJacobPazos/AsturiasAviles.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.IsaacJacobPazos;
+
+public class AsturiasAviles {
+
+	public static void main(String[] args) {
+			String[] playas={"de Bañuegues","de Tenrero","de Cerniciega","Aguilera","de Xago","del Espartal","de Salinas","de Arnao","Santa Maria del Mar","Bahinas","de Bayas","Los Quebrantos","Aguilar","Concha de Artedo","De San Pedro","Juan de Nieva","La Mina"};
+			System.out.println("Listado de todas las playas de Asturias, Aviles");
+			for (int i = 0; i < playas.length; i++) {
+				System.out.println("Playa "+playas[i]);
+			}
+	}
+
+}
