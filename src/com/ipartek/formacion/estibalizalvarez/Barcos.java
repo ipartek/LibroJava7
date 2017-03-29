5f2f150b60846ad7c6dc6220cb65b406a29eaa01
Estibaliz <Estibaliz@Estibaliz>
2017-03-29 12:59:40 +0200

    Mi ejercicio de barcos empezado.

---
+++ src/com/ipartek/formacion/estibalizalvarez/Barcos.java
@@ -0,0 +1,20 @@
+package com.ipartek.formacion.estibalizalvarez;
+
+public class Barcos {
+	public static void main(String[] args) {
+		char[][]tablero;
+		tablero=new char [4][4];
+		int f;
+		int c;
+		f=0;
+		c=0;
+		
+		
+		
+		
+	}
+	
+	
+	
+
+}
