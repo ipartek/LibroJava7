6a264df297ea86c9e8c2809139f14848d5bad948
javierlete <jglete@gmail.com>
2017-03-29 16:14:47 +0200

    Clase Punto con encapsulación

---
+++ src/com/ipartek/formacion/javierlete/ejemplopoo/tipos/Punto.java
@@ -0,0 +1,37 @@
+package com.ipartek.formacion.javierlete.ejemplopoo.tipos;
+
+public class Punto {
+	private double x, y;
+
+	public int getX() {
+		return (int) x;
+	}
+
+	public void setX(int x) {
+		this.x = (double) x;
+	}
+
+	public int getY() {
+		return (int) y;
+	}
+
+	public void setY(int y) {
+		this.y = (double) y;
+	}
+
+	public double getXDouble() {
+		return x;
+	}
+
+	public double getYDouble() {
+		return y;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+}
