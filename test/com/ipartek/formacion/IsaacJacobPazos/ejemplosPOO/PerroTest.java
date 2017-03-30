e783884a35d3ec7b10b139799f48bc128eb42d97
IJPazos <saga.1516@gmail.com>
2017-03-30 18:26:36 +0200

    Acabados tests de personas y coches.

--- test/com/ipartek/formacion/IsaacJacobPazos/ejemplosPOO/PerroTest.java
+++ test/com/ipartek/formacion/IsaacJacobPazos/ejemplosPOO/PerroTest.java
@@ -1,61 +1,109 @@
 package com.ipartek.formacion.IsaacJacobPazos.ejemplosPOO;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.ipartek.formacion.IsaacJacobPazos.ejemplosPOO.coches.Perro;
 
 public class PerroTest {
+	Perro p, p2;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		// se ejecuta al iniciar Clase de Test
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		// se ejecuta al finalizar Clase de Test
+
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		// Se ejecuta antes de cada @Test
+		p = new Perro("GUAU");
+		p2 = new Perro("Pedos", "JamonYorkside y queso", "Hot man", true);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		// Se ejecuta despues de cada @Test
+
+		// sin parametros
+		p = null;
+		// con parametros
+		p2 = null;
+	}
 
 	@Test
 	public void testPerroString() {
-		Perro p = new Perro("");
+		assertNotNull(p.getNombre());
+
+		assertNotNull(p2.getNombre());
 	}
 
 	@Test
 	public void testPerroStringStringStringBoolean() {
-		fail("Not yet implemented");
+		assertNotNull(p.getNombre());
+		assertNotNull(p.getDueño());
+		assertNotNull(p.getRaza());
+		assertNotNull(p.isPulga());
+
+		assertNotNull(p2.getNombre());
+		assertNotNull(p2.getDueño());
+		assertNotNull(p2.getRaza());
+		assertNotNull(p2.isPulga());
 	}
 
 	@Test
 	public void testGetNombre() {
-		fail("Not yet implemented");
+		assertEquals("GUAU", p.getNombre());
 	}
 
 	@Test
 	public void testSetNombre() {
-		fail("Not yet implemented");
+		p.setNombre("Cohete");
+		assertEquals("Cohete", p.getNombre());
 	}
 
 	@Test
 	public void testGetRaza() {
-		fail("Not yet implemented");
+		assertEquals("Birraza", p.getRaza());
 	}
 
 	@Test
 	public void testSetRaza() {
-		fail("Not yet implemented");
+		p.setRaza("Perro pato");
+		assertEquals("Perro pato", p.getRaza());
 	}
 
 	@Test
 	public void testGetDueño() {
-		fail("Not yet implemented");
+		assertEquals("NoDueño", p.getDueño());
 	}
 
 	@Test
 	public void testSetDueño() {
-		fail("Not yet implemented");
+		p.setDueño("pedotense");
+		assertEquals("pedotense", p.getDueño());
 	}
 
 	@Test
 	public void testIsPulga() {
-		fail("Not yet implemented");
+		assertEquals(false, p.isPulga());
 	}
 
 	@Test
 	public void testSetPulga() {
-		fail("Not yet implemented");
+		p.setPulga(true);
+		assertEquals(true, p.isPulga());
 	}
 
 }
