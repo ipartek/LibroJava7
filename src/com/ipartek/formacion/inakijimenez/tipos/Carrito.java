8e284ff245df6c231c128328464e259a1440ef38
JAVA <JAVA@PORT-Z12>
2017-04-03 19:31:41 +0200

    Ejercicio Carrito

---
+++ src/com/ipartek/formacion/inakijimenez/tipos/Carrito.java
@@ -0,0 +1,42 @@
+package com.ipartek.formacion.inakijimenez.tipos;
+
+import java.util.ArrayList;
+
+public class Carrito {
+
+	private ArrayList<Producto> carrito = new ArrayList<>();
+
+	public void add(Producto producto) {
+		carrito.add(producto);
+	}
+
+	@Override
+	public String toString() {
+		String carro = "";
+		for (Producto producto : carrito) {
+			carro = carro + producto.toString();
+		}
+		return "Carrito\n" + carro;
+	}
+
+	public int getNumeroArticulos() {
+		return carrito.size();
+	}
+
+	public double getPrecioTotal() {
+		double precioTotal = 0.0;
+		for (Producto producto : carrito) {
+			precioTotal += producto.getPrecio();
+		}
+		return precioTotal;
+	}
+
+	public double getIvaSobrePrecioTotal() {
+		return getPrecioTotal() * 0.21;
+	}
+
+	public double getPrecioTotalConIva() {
+		return (getPrecioTotal() + getIvaSobrePrecioTotal());
+	}
+
+}
