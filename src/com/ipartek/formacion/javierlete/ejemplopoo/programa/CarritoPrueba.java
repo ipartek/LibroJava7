d81ce490455cb55d85a24f811c3bfd7c7da3c8bb
javierlete <jglete@gmail.com>
2017-04-04 16:24:52 +0200

    Ejercicio Carrito primera parte

--- src/com/ipartek/formacion/javierlete/ejemplopoo/programa/CarritoPrueba.java
+++ src/com/ipartek/formacion/javierlete/ejemplopoo/programa/CarritoPrueba.java
@@ -1,5 +1,8 @@
 package com.ipartek.formacion.javierlete.ejemplopoo.programa;
 
+import com.ipartek.formacion.javierlete.ejemplopoo.tipos.Carrito;
+import com.ipartek.formacion.javierlete.ejemplopoo.tipos.Producto;
+
 public class CarritoPrueba {
 
 	public static void main(String[] args) {
@@ -7,9 +10,11 @@ public class CarritoPrueba {
 
 		c.add(new Producto(1, "Monitor 21\"", 125.03));
 		c.add(new Producto(2, "Placa base ...", 100.2));
-		c.add(new Producto(3, "Ratón", 10.3));
+		c.add(new Producto(30, "Ratón", 10.3));
 
 		System.out.println(c);
+
+		System.out.println(c.toString(true));
 
 		/*
 		 * ID: 1, Monitor 21", 125.03€ ...
