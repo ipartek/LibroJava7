36117388fe1b18757d657f1e6aef7b678573b500
daniel <daniel@Daniel>
2017-04-04 12:53:42 +0200

    Carrito_Ok

--- src/com/ipartek/formacion/danielsan/Carrito.java
+++ src/com/ipartek/formacion/danielsan/Carrito.java
@@ -3,10 +3,52 @@ package com.ipartek.formacion.danielsan;
 import java.util.ArrayList;
 
 public class Carrito {
-	private ArrayList<Producto> producto;
+	private ArrayList<Producto> Producto = new ArrayList<Producto>();
 
+	//Constructores
+	public Carrito(ArrayList<com.ipartek.formacion.danielsan.Producto> producto) {
+		super();
+		Producto = producto;
+	}
+	
 	public Carrito() {
 		super();
-		this.producto = producto;
+	}
+
+
+	//Setters y Getters
+	public ArrayList<Producto> getProducto() {
+		return Producto;
+	}
+
+	public void setProducto(ArrayList<Producto> producto) {
+		Producto = producto;
+	}
+	
+	//Metodos
+	public int getNumeroDeArticulos()	{
+		return Producto.size();
+	}
+	public double getPrecioTotal(){
+		double Precio=0;
+		for(int i=0; i < Producto.size();i++){
+			Precio=Precio + Producto.get(i).getPrecio();
+		}
+		return Precio;
+	}
+	public double getIvaSobrePrecioTotal(){
+		double Precio=getPrecioTotal();
+		return Precio*0.21;
+	}
+	public double getPrecioTotalConIva(){
+		double Precio=getPrecioTotal();
+		Precio=Precio+(Precio*0.21);
+		return Precio;
+	}
+	
+	//ToString
+	@Override
+	public String toString() {
+		return "Carrito [Producto=" + Producto + "]";
 	}
 }
--- src/com/ipartek/formacion/danielsan/Producto.java
+++ src/com/ipartek/formacion/danielsan/Producto.java
@@ -2,14 +2,50 @@ package com.ipartek.formacion.danielsan;
 
 public class Producto {
 
-	private String[] Carrito;
+	private int indice;
+	private String nombre;
+	private double precio;
 
+	//Constructores
 	public Producto(int indice, String nombre, double precio) {
 		super();
-
-		this.Carrito[0].valueOf(indice);
-		this.Carrito[1] = nombre;
-		this.Carrito[2].valueOf(precio);
+		this.indice = indice;
+		this.nombre = nombre;
+		this.precio = precio;
+	}
+	
+	public Producto() {
+		super();
+	}
+	
+	//Setters y Getters
+	public int getIndice() {
+		return indice;
 	}
 
+	public void setIndice(int indice) {
+		this.indice = indice;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	
+	//ToString
+	@Override
+	public String toString() {
+		return "\nProducto [indice=" + indice + ", nombre=" + nombre + ", precio=" + precio + "]";
+	}
 }
