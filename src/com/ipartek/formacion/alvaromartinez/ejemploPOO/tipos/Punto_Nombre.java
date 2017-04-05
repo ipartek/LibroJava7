c91d6edfd6136c31f587a6772b0596369acb31f9
Amartinezalbala <martinezalbalaalvaro@gmail.com>
2017-04-05 20:01:58 +0200

    Herencia

---
+++ src/com/ipartek/formacion/alvaromartinez/ejemploPOO/tipos/Punto_Nombre.java
@@ -0,0 +1,14 @@
+package com.ipartek.formacion.alvaromartinez.ejemploPOO.tipos;
+
+public class Punto_Nombre extends Punto {
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
