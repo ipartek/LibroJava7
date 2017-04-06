936de6be5e61e5810d7f14e357b724fea8105c41
javierlete <jglete@gmail.com>
2017-04-06 14:50:31 +0200

    0 Warnings

--- src/com/ipartek/formacion/javierlete/ejemplopoo/programa/CarritoPrueba.java
+++ src/com/ipartek/formacion/javierlete/ejemplopoo/programa/CarritoPrueba.java
@@ -37,8 +37,8 @@ public class CarritoPrueba {
 		System.out.println(c.getIvaSobrePrecioTotal()); // Asumimos IVA 21%
 		System.out.println(c.getPrecioTotalConIva());
 
-		double precio = 125.03;
-		BigDecimal precioNuevo = new BigDecimal(125.03);
+		//double precio = 125.03;
+		//BigDecimal precioNuevo = new BigDecimal(125.03);
 
 	}
 }
